3164c551e651757729a9acc047c6f55c583f3085
aasamelon <ayamelody386@gmail.com>
2025-06-09 15:19:35 +0900

    コントローラーにcreateアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
   end
 
+   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end 
+
 end
