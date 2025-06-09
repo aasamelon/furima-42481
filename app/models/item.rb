a40ce17483f1cbbc19da8f65fd4c53ec4242ac18
aasamelon <ayamelody386@gmail.com>
2025-06-09 14:18:50 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
- belongs_to :user
- has_one :order
- has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_one :order
+  has_one_attached :image
+  belongs_to :category
 
 end
