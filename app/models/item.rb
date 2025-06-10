3144144d65d2e2bfd0f328f35616386b9a99231d
aasamelon <ayamelody386@gmail.com>
2025-06-10 20:33:05 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,24 +22,10 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, numericality: { other_than: 1, message: "を選択してください" }
   validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
   validates :shipping_day_id, numericality: { other_than: 1, message: "を選択してください" }
-
-  validate :price_must_be_number_and_in_range
-
-  private
-
   validates :price, numericality: {
     only_integer: true,
     greater_than_or_equal_to: 300,
     less_than_or_equal_to: 9_999_999,
     message: "は¥300〜¥9,999,999の間で入力してください"
    }
-  end
-
-    num = price.to_i
-    if num < 300
-      errors.add(:price, "は¥300〜¥9,999,999の間で入力してください")
-    elsif num > 9_999_999
-      errors.add(:price, "は¥300〜¥9,999,999の間で入力してください")
-    end
-  end
 end
