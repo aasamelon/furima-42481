2b76e0317bfea3700645f2ecc8a7f4aeb250350a
aasamelon <ayamelody386@gmail.com>
2025-06-10 21:53:15 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "furimas#index"
 
-  resources :items, only: [:new, :create, :edit]
+  resources :items, only: [:new, :create, :edit,:index]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
