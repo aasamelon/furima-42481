2c18b71c28d5df701ca2a757d755dc8a661185a5
aasamelon <ayamelody386@gmail.com>
2025-06-13 16:24:09 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
 
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :items  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
