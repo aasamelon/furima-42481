70c1210a54f0df727aa214ac332f93f7d1b4d8a8
aasamelon <ayamelody386@gmail.com>
2025-06-17 15:21:15 +0900

    テストコードを実装

--- app/models/order_shipping_form.rb
+++ app/models/order_shipping_form.rb
@@ -7,23 +7,30 @@ class OrderShippingForm
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code
-    validates :prefecture_id
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city
     validates :address
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
   end
 
+  validates :prefecture_id, presence: true
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+
   def save
-    order = Order.create(user_id: user_id, item_id: item_id)
-    Shipping.create(
-      order_id: order.id,
-      postal_code: postal_code,
-      prefecture_id: prefecture_id,
-      city: city,
-      address: address,
-      building_name: building_name,
-      phone_number: phone_number
-    )
+    ActiveRecord::Base.transaction do
+      order = Order.create!(user_id: user_id, item_id: item_id)
+      Shipping.create!(
+        order_id: order.id,
+        postal_code: postal_code,
+        prefecture_id: prefecture_id,
+        city: city,
+        address: address,
+        building_name: building_name,
+        phone_number: phone_number
+      )
+    end
+    true
+  rescue ActiveRecord::RecordInvalid
+    false
   end
 end
