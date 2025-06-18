8d001b925be3138aff2ae0ba8b3005817edcd7bb
aasamelon <ayamelody386@gmail.com>
2025-06-18 18:42:48 +0900

    修正

--- spec/factories/order_shipping_forms.rb
+++ spec/factories/order_shipping_forms.rb
@@ -8,24 +8,5 @@ FactoryBot.define do
     building_name { 'マンション101' }
     phone_number  { '09012345678' }
     token         { 'tok_abcdefghijk00000000000000000' }
-
-    transient do
-      user { nil }
-      item { nil }
-    end
-
-    initialize_with do
-      OrderShippingForm.new(
-        postal_code: postal_code,
-        prefecture_id: prefecture_id,
-        city: city,
-        address: address,
-        building_name: building_name,
-        phone_number: phone_number,
-        token: token,
-        user_id: user&.id,
-        item_id: item&.id
-      )
-    end
   end
 end
