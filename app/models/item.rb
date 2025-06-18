b15113624f29cbe5269215a21e56d60b37fac17a
aasamelon <ayamelody386@gmail.com>
2025-06-19 05:44:15 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,26 +10,25 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
-  with_options presence: true do
+  with_options presence: { message: "can't be blank" } do
     validates :image
     validates :name
     validates :description
     validates :price
   end
 
-  validates :category_id, numericality: { other_than: 1, message: "を選択してください" }
-  validates :condition_id, numericality: { other_than: 1, message: "を選択してください" }
-  validates :shipping_fee_id, numericality: { other_than: 1, message: "を選択してください" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
-  validates :shipping_day_id, numericality: { other_than: 1, message: "を選択してください" }
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
+
   validates :price, numericality: {
     only_integer: true,
     greater_than_or_equal_to: 300,
     less_than_or_equal_to: 9_999_999,
-    message: "は¥300〜¥9,999,999の間で入力してください"
-   }
-
-    #def sold_out?
-    #order.present?
-  #end
+    message: "is not a number"
+  }
 end
