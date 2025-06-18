d90201df387c3e9142b6bd5270524dfcaa664aad
aasamelon <ayamelody386@gmail.com>
2025-06-18 16:29:06 +0900

    修正

--- app/models/order_shipping_form.rb
+++ app/models/order_shipping_form.rb
@@ -4,18 +4,20 @@ class OrderShippingForm
   attr_accessor :user_id, :item_id,
                 :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token
 
-  with_options presence: true do
-    validates :user_id
-    validates :item_id
-    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :city
-    validates :address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
-    validates :token
-    validates :prefecture_id
-  end
+validates :postal_code, presence: { message: "can't be blank" },
+                         format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter as 123-4567" }
 
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+validates :prefecture_id, numericality: { other_than: 0, message: "must be selected" }
+
+validates :city, presence: { message: "can't be blank" }
+
+validates :address, presence: { message: "can't be blank" }
+
+validates :phone_number, presence: { message: "can't be blank" },
+                         format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only numbers" }
+
+validates :token, presence: { message: "can't be blank" }
+
 
   def save
     ActiveRecord::Base.transaction do
