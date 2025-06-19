9306f39df740c86363caa3fd93328056550d604e
aasamelon <ayamelody386@gmail.com>
2025-06-19 16:12:17 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -39,16 +39,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_15_233345) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "categories", charset: "utf8mb3", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "conditions", charset: "utf8mb3", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "items", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
